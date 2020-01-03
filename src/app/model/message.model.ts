b2d84020f73909f6c232ea3e355277f943590871
shakil-576 <shakil576.developer@gmail.com>
2020-01-03 10:55:48 +0530

    Added message model

---
+++ src/app/model/message.model.ts
@@ -0,0 +1,10 @@
+export interface Message {
+    salutation?: string
+    fname?: string,
+    lname?: string,
+    email?: string,
+    phoneNumber?: string,
+    ourServices?: string,
+    message?: string,
+    timestamp?: Date
+}
