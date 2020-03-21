92fbf0aee378152c70e9e5b0e174f8e803502e02
Shakil Ansari <shakil576.developer@gmail.com>
2020-03-21 13:54:53 +0530

    fix wrong header

--- src/app/work-strategy/work-strategy.component.ts
+++ src/app/work-strategy/work-strategy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-work-strategy',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkStrategyComponent implements OnInit {
 
+  brandName = environment.BrandName;
+  
   constructor() { }
 
   ngOnInit() {
